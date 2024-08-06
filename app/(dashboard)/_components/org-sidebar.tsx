366724b2d203bb5529bd9d62e374b03acde1d01d
BadriVishalPadhy <128573881+BadriVishalPadhy@users.noreply.github.com>
2024-08-06 22:54:23 +0530

    commit

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -4,8 +4,7 @@
 
   return(
 
-    <div className=" invisible lg:visible lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5  bg-red-800  h-full          
-    ">
+    <div className=" invisible lg:visible lg:flex   flex-col space-y-6 w-[206px] pl-5 pt-5  h-full bg-red-800  ">
       Org-Sidebar
     </div>
   );
